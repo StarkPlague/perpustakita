6d14d098b6009d795a9eb3f0699d37b86165e8a6
faiq <sutiono.plague@gmail.com>
2025-05-26 17:49:32 +0700

    connect db, add dummy books, rendex html

--- go.mod
+++ go.mod
@@ -1,7 +1,11 @@
-module github.com/StarkPlague/perpustakita
+module perpustakita
 
 go 1.24.3
 
-require github.com/lib/pq v1.10.9
-
-require github.com/gorilla/mux v1.8.1 // indirect
+require (
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
+	github.com/jackc/pgx/v5 v5.7.5 // indirect
+	golang.org/x/crypto v0.37.0 // indirect
+	golang.org/x/text v0.24.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,21 +1,54 @@
 package main
 
 import (
-	"log"
+	"fmt"
+	"html/template"
 	"net/http"
 
-	"github.com/StarkPlague/perpustakita/db"
-	"github.com/gorilla/mux"
+	"context"
+	"os"
+
+	"github.com/jackc/pgx/v5"
 )
 
+var db *pgx.Conn
+
+func connectDB() {
+	var err error
+	db, err = pgx.Connect(context.Background(), "postgres://postgres:1212@localhost:5432/perpustakita")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		os.Exit(1)
+	}
+	fmt.Println("Connected to DB!")
+}
+
+func insertDummyBook() {
+	_, err := db.Exec(context.Background(),
+		"INSERT INTO books (title, author, quantity) VALUES  ($1, $2, $3)",
+		"Atomic Habits", "James Clear", 5,
+	)
+	if err != nil {
+		fmt.Println("insert error: ", err)
+	} else {
+		fmt.Println("New dummy books inserted")
+	}
+}
+
 func main() {
-	db.Connect()
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		tmpl, err := template.ParseFiles("templates/index.html")
 
-	r := mux.NewRouter()
-	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("p!"))
+		if err != nil {
+			fmt.Println(err)
+			http.Error(w, "error parsing template", http.StatusInternalServerError)
+			return
+		}
+		tmpl.Execute(w, nil)
 	})
+	connectDB()
+	insertDummyBook()
+	fmt.Println("Server Running on http://localhost:8080")
+	http.ListenAndServe(":8080", nil)
 
-	log.Println("Server running on :8080")
-	http.ListenAndServe(":8080", r)
 }
