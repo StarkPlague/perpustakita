17971e4e78b86636c89f90900487688659b122c0
faiq <sutiono.plague@gmail.com>
2025-05-27 22:18:55 +0700

    adding function addBookHandler & indexHandler, update index.html by adding form to submit books

--- main.go
+++ main.go
@@ -13,6 +13,47 @@ import (
 
 var db *pgx.Conn
 
+func addBookHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	title := r.FormValue("title")
+	author := r.FormValue("author")
+	quantityStr := r.FormValue("quantity")
+
+	var quantity int
+	_, err := fmt.Sscan(quantityStr, &quantity)
+	if err != nil {
+		http.Error(w, "Invalid quantity", http.StatusBadRequest)
+		return
+	}
+
+	_, err = db.Exec(context.Background(),
+		"INSERT INTO books (title, author, quantity) VALUES ($1, $2, $3)",
+		title, author, quantity,
+	)
+	if err != nil {
+		fmt.Println("Insert error: ", err)
+		http.Error(w, "Failed inserting books", http.StatusInternalServerError)
+		return
+	}
+
+	http.Redirect(w, r, "/", http.StatusSeeOther)
+
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl, err := template.ParseFiles("templates/index.html")
+	if err != nil {
+		http.Error(w, "Template error", http.StatusInternalServerError)
+		return
+	}
+
+	tmpl.Execute(w, nil)
+}
+
 func connectDB() {
 	var err error
 	db, err = pgx.Connect(context.Background(), "postgres://postgres:1212@localhost:5432/perpustakita")
@@ -36,18 +77,10 @@ func insertDummyBook() {
 }
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		tmpl, err := template.ParseFiles("templates/index.html")
-
-		if err != nil {
-			fmt.Println(err)
-			http.Error(w, "error parsing template", http.StatusInternalServerError)
-			return
-		}
-		tmpl.Execute(w, nil)
-	})
 	connectDB()
-	insertDummyBook()
+	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/add-book", addBookHandler)
+	//	insertDummyBook()
 	fmt.Println("Server Running on http://localhost:8080")
 	http.ListenAndServe(":8080", nil)
 
