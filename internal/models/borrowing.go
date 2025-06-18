a63c3949e91ba71b431479d273ef2c61a0b1c945
faiq <sutiono.plague@gmail.com>
2025-06-18 23:54:40 +0700

    building borrowing models

---
+++ internal/models/borrowing.go
@@ -0,0 +1,25 @@
+package models
+
+import (
+	"context"
+	"github.com/jackc/pgx/v5"
+)
+
+type Borrowing struct {
+	ID              int
+	BorrowerName    string
+	BorrowerContact string
+	BookID          string
+	BorrowerDate    string
+	Status          string
+}
+
+func InsertBorrowing(ctx context.Context, conn *pgx.Conn, b Borrowing) error {
+	// insert borrowing record
+	_, err:= conn.Exec(ctx,`
+	INSERT INTO borrowings (borrower_name, borrower_contact, book_id)
+	values ($1,$2,$3)`,
+b.BorrowerName, b.BorrowerContact,b.BookID)
+
+
+}
