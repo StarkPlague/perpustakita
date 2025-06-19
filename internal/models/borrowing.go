034ba211e08b479aaf6bbf686185eea50c53cdd0
faiq <sutiono.plague@gmail.com>
2025-06-19 23:31:06 +0700

    update func InsertBorrowing borrowing models

--- internal/models/borrowing.go
+++ internal/models/borrowing.go
@@ -2,6 +2,7 @@ package models
 
 import (
 	"context"
+
 	"github.com/jackc/pgx/v5"
 )
 
@@ -16,10 +17,21 @@ type Borrowing struct {
 
 func InsertBorrowing(ctx context.Context, conn *pgx.Conn, b Borrowing) error {
 	// insert borrowing record
-	_, err:= conn.Exec(ctx,`
+	_, err := conn.Exec(ctx, `
 	INSERT INTO borrowings (borrower_name, borrower_contact, book_id)
 	values ($1,$2,$3)`,
-b.BorrowerName, b.BorrowerContact,b.BookID)
+		b.BorrowerName, b.BorrowerContact, b.BookID)
 
+	if err != nil {
+		return err
+	}
+
+	//reduce stock
+	_, err = conn.Exec(ctx, `
+	UPDATE books SET quantity = quantity - 1
+	where id = $1 AND quantity > 0`,
+		b.BookID)
+
+	return err
 
 }
